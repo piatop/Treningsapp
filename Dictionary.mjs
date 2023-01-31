ab36187bccc38f3434b5ed2201e8b96c30797779
Piaj <piajtop@gmail.com>
2023-01-31 15:19:02 +0100

    første modul

---
+++ Dictionary.mjs
@@ -0,0 +1,34 @@
+
+class Dictionary{
+
+    constructor(...languages){
+        this.languagefiles = languages
+        this.dictionary = null;
+        this.currentLanguage = null
+    }
+
+    setLanguage = async function(lang){
+        // Velger språket som skal brukes.
+
+        let languageFile = this.languagefiles.find( file => {
+             return file.indexOf(lang) != -1
+            });
+        
+        this.currentLanguage = lang;
+
+        this.dictionary = await import(languageFile, { assert: { type: "json" } });
+    }
+
+    get = function(key){
+        // Henter riktig verdi basert på key i dictionary.
+        return this.dictionary[key];
+    }
+
+    keys = function(){
+        // retunerer alle keys i dictionary 
+        return Object.keys( this.dictionary);
+    }
+}
+
+
+export default Dictionary;
---
+++ test/testLoadingLanguages.mjs
@@ -0,0 +1,34 @@
+import Dictionary from "../Dictionary.mjs"
+
+(function runTests(){
+    testCreatingLanguageInterface();
+    testSuportingLanguageFiles();
+})();
+
+
+function testCreatingLanguageInterface(){
+    const dictionary = new Dictionary();
+    test(dictionary != undefined, "Creating instance of Language suport module");
+}
+
+function testSuportingLanguageFiles(){
+    const languages = ["./lan/no.json", "./lan/en.json"];
+    const dictionary = new Dictionary(...languages);
+    test(JSON.stringify(dictionary.languagefiles) === JSON.stringify(languages), "Testing setting of languages");
+}
+
+function testLoadingLangaugeResources(){
+    const languages = ["./lan/no.json", "./lan/en.json"];
+    const dictionary = new Dictionary(...languages);
+    dictionary.setLanguage("no");
+    test(JSON.stringify(dictionary.languagefiles) === JSON.stringify(languages), "Testing setting of languages");
+}
+
+
+function test(test, description){
+    if(test){
+        console.log(`🟢 ${description}`)
+    } else{
+        console.log(`🔴 ${description},  ${test}`)
+    }
+}
