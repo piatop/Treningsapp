e9919073fa361cefc709b9f6d93a0e32f2e38da4
Piaj <piajtop@gmail.com>
2023-02-07 16:10:13 +0100

    commiter innlevering pia

--- Dictionary.mjs
+++ Dictionary.mjs
@@ -1,10 +1,13 @@
 
+import en from './test/lan/en.json' assert { type: 'json' };
+import no from './test/lan/no.json' assert { type: 'json' };
+
 class Dictionary{
 
     constructor(...languages){
         this.languagefiles = languages
         this.dictionary = null;
-        this.currentLanguage = null
+        this.currentLanguage = null;
     }
 
     setLanguage = async function(lang){
@@ -21,14 +24,26 @@ class Dictionary{
 
     get = function(key){
         // Henter riktig verdi basert på key i dictionary.
+        //console.log(this.dictionary.key[]);
         return this.dictionary[key];
     }
 
+
     keys = function(){
         // retunerer alle keys i dictionary 
-        return Object.keys( this.dictionary);
+        return Object.keys(this.dictionary);
     }
+    
 }
+
+let DICTIONARY_KEYS = {
+    end:"end"
+}
+
+let dictionary = new Dictionary('./test/lan/');
+dictionary.setLanguage(no);
+dictionary.get(DICTIONARY_KEYS.end);
+console.log(dictionary.currentLanguage);
 
 
 export default Dictionary;
